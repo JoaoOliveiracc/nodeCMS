7f37493471cc27088e86bd3c6e03cb9d20c14071
João Monteiro <ccjoao_monteiro@outlook.com>
2021-04-15 13:41:28 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const connection = require("./database/database");
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+connection
+    .authenticate()
+    .then(() => {
+        console.log("Conexão com o banco realizada!")
+    }).catch((error) => {
+        console.log(error);
+    });
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.listen(3000, () => {
+    console.log("Servidor iniciado!");
+});
