e2324e63f747cf4d1567660db9881b6bb080d71e
João Monteiro <ccjoao_monteiro@outlook.com>
2021-04-26 12:38:01 -0300

    Saving articles

--- database/database.js
+++ database/database.js
@@ -2,7 +2,8 @@ const Sequelize = require("sequelize");
 
 const connection = new Sequelize("node_cms", "root" ,"", {
     host: "localhost",
-    dialect: "mysql"
+    dialect: "mysql",
+    timezone: "-03:00"
 });
 
 module.exports = connection;
