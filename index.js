9597c51434c237571d4d4a87fbd8f6822aeb5e83
João Monteiro <ccjoao_monteiro@outlook.com>
2021-04-27 12:24:23 -0300

    Link with article

--- index.js
+++ index.js
@@ -33,6 +33,24 @@ app.get("/", (req, res) => {
     });
 });
 
+app.get("/:slug", (req, res) => {
+    var slug = req.params.slug;
+
+    Article.findOne({
+        where: {
+            slug: slug
+        }
+    }).then((article) => {
+        if(article != undefined) {
+            res.render("article", {article: article});
+        }else {
+            res.redirect("/");
+        }
+    }).catch(erro => {
+        res.redirect("/");
+    });
+});
+
 app.listen(3000, () => {
     console.log("Servidor iniciado!");
 });
