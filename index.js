06a2c6bdc0f1e07427176367e55c817ec50e10ee
João Monteiro <ccjoao_monteiro@outlook.com>
2021-04-28 08:33:52 -0300

    Browsing categories

--- index.js
+++ index.js
@@ -59,6 +59,27 @@ app.get("/:slug", (req, res) => {
     });
 });
 
+app.get("/category/:slug", (req, res) => {
+    var slug = req.params.slug;
+
+    Category.findOne({
+        where: {
+            slug: slug
+        },
+        include: [{model: Article}]
+    }).then((category) => {
+        if(category != undefined) {
+            Category.findAll().then(categories => {
+                res.render("index", {articles: category.articles, categories: categories});
+            });
+        }else {
+            res.redirect("/");
+        }
+    }).catch(erro => {
+        res.redirect("/");
+    });
+})
+
 app.listen(3000, () => {
     console.log("Servidor iniciado!");
 });
