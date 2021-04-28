f905eb6cb43bce7b18e8caf7bdb89a2e984e115b
João Monteiro <ccjoao_monteiro@outlook.com>
2021-04-28 08:15:33 -0300

    Categories in the navbar

--- index.js
+++ index.js
@@ -28,8 +28,14 @@ app.use("/", categoriesController);
 app.use("/", articlesController);
 
 app.get("/", (req, res) => {
-    Article.findAll().then((articles) => {
-        res.render("index", {articles: articles});
+    Article.findAll({
+        order: [
+            ["id", "DESC"]
+        ]
+    }).then((articles) => {
+        Category.findAll().then((categories) => {
+            res.render("index", {articles: articles, categories: categories});
+        });
     });
 });
 
@@ -42,7 +48,9 @@ app.get("/:slug", (req, res) => {
         }
     }).then((article) => {
         if(article != undefined) {
-            res.render("article", {article: article});
+            Category.findAll().then((categories) => {
+                res.render("article", {article: article, categories: categories});
+            });
         }else {
             res.redirect("/");
         }
