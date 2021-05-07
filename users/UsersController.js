6ddc01995bc23934b47764028c0b48e7ee36718e
João Monteiro <ccjoao_monteiro@outlook.com>
2021-05-07 08:36:59 -0300

    Register user with hash password

--- users/UsersController.js
+++ users/UsersController.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("./User");
+const bcrypt = require("bcryptjs");
 
 router.get("/admin/users", (req, res) => {
     res.send("Listagem de usuários");
@@ -13,8 +14,17 @@ router.get("/admin/users/create", (req, res) => {
 router.post("/users/create", (req, res) => {
     var email = req.body.email;
     var password = req.body.password;
+    var salt = bcrypt.genSaltSync(10);
+    var hash = bcrypt.hashSync(password, salt);
 
-    res.json({email, password});
+    User.create({
+        email: email,
+        password: hash
+    }).then(() => {
+        res.json({message: "Cadastrado com sucesso!"});
+    }).catch((erro) => {
+        res.resirect("/");
+    });
 });
 
 module.exports = router;
